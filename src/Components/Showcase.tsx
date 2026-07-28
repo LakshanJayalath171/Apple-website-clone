ddfbe9b703b91988464ee32b91a08c0cfeb829ad
LakshanJayalath171 <lakshanjayalath171@gmail.com>
2026-07-28 15:56:22 +0530

    Created the animated video section

--- src/Components/Showcase.tsx
+++ src/Components/Showcase.tsx
@@ -1,13 +1,40 @@
+import { useMediaQuery } from "react-responsive";
+import {useGSAP} from '@gsap/react'
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+
 const Showcase = () => {
+
+    const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+    gsap.registerPlugin(ScrollTrigger,useGSAP);
+    
+    useGSAP(()=>{
+     if(!isTablet){
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#showcase",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+          pin: true,
+        },
+      });
+
+      tl.to(".mask img", { transform: 'scale(1.2)' }).to(".content", { opacity: 1, y: 0, ease:'power1.in' });
+     }
+
+    },[isTablet])
+
+
   return (
     <section id="showcase">
       <div className="media">
-            <video src="/videos/game.mp4" loop muted autoPlay/>
+        <video src="/videos/game.mp4" loop muted autoPlay />
 
-            <div>
-                <img src="/mask-logo.svg"/>
-            </div>
+        <div className="mask">
+          <img src="/mask-logo.svg" />
         </div>
+      </div>
 
       <div className="">
         <div className="wrapper">
